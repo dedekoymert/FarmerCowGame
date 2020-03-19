7242d04dbc17a00768838998ce19de55e4a3860e
dedekoymert <mert.dedekoy@ozu.edu.tr>
2020-03-19 15:19:19 +0300

    one turn passed events added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,24 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
 
-//    String mapPath = args[0];
-//    int cowNumber = Integer.parseInt(args[1]);
-//    int farmerNumber = Integer.parseInt(args[2]);
-//    boolean stop = true;
-//    if (args[3].equals("false")) {
-//      stop = false;
-//    }
-//    int foodNumber = Integer.parseInt(args[4]);
+    for (int i = 0; i < args.length; i++){
 
-    String mapPath = "/Users/mertdedekoy/Documents/dev/FarmerCowGame/map";
-    int cowNumber = 4;
-    int farmerNumber = 2;
+    }
+
+    String mapPath = args[0];
+    int cowNumber = Integer.parseInt(args[1]);
+    int farmerNumber = Integer.parseInt(args[2]);
     boolean stop = true;
-    int foodNumber = 3;
+    if (args[3].equals("false")) {
+      stop = false;
+    }
+    int foodNumber = Integer.parseInt(args[4]);
+
+//    String mapPath = "/Users/mertdedekoy/Documents/dev/FarmerCowGame/map";
+//    int cowNumber = 4;
+//    int farmerNumber = 2;
+//    boolean stop = true;
+//    int foodNumber = 3;
 
     new App(mapPath, cowNumber, farmerNumber, foodNumber, stop);
 
