7b6257ee093205fb8154ae5fe8ffc71fc9806828
dedekoymert <mert.dedekoy@ozu.edu.tr>
2020-03-19 16:21:31 +0300

    input is handled

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,42 @@
 package com.company;
 
 import java.io.*;
+import java.util.InputMismatchException;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
 
-    for (int i = 0; i < args.length; i++){
-
-    }
-
-    String mapPath = args[0];
-    int cowNumber = Integer.parseInt(args[1]);
-    int farmerNumber = Integer.parseInt(args[2]);
+    String mapPath = "";
+    int cowNumber = 0;
+    int farmerNumber = 0;
     boolean stop = true;
-    if (args[3].equals("false")) {
-      stop = false;
+    int foodNumber = 0;
+
+    for (int i = 0; i < args.length; i++){
+      if (args[i].equals("-map")){
+        i++;
+        mapPath = "/Users/mertdedekoy/Documents/dev/FarmerCowGame/map";
+      } else if (args[i].equals("-cows")) {
+        i++;
+        cowNumber = Integer.parseInt(args[i]);
+      } else if (args[i].equals("-farmer")) {
+        i++;
+        farmerNumber = Integer.parseInt(args[i]);
+      } else if (args[i].equals("-food")) {
+        i++;
+        foodNumber = Integer.parseInt(args[i]);
+      } else if (args[i].equals("-stop")) {
+        i++;
+        if (args[i].equals("false")){
+          stop = false;
+        }
+      } else {
+        return;
+      }
     }
-    int foodNumber = Integer.parseInt(args[4]);
+
+
 
 //    String mapPath = "/Users/mertdedekoy/Documents/dev/FarmerCowGame/map";
 //    int cowNumber = 4;
